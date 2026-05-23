d2046deecc5486290c111ef88a65edafba9eaa67
ilham putra <ilhamptr007@gmail.com>
2026-05-23 20:03:04 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, Response
+from models.frames import generate_frames
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/video')
+def video():
+    return Response(
+        generate_frames(),
+        mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
